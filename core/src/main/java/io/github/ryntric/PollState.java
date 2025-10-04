02607116ed8e1cc3ca2cf3eb12142145600afa52
ryntric <io.github.ryntric@gmail.com>
2025-10-04 19:53:06 +0300

    reworked to channel architecture

--- core/src/main/java/io/github/ryntric/PollState.java
+++ core/src/main/java/io/github/ryntric/PollState.java
@@ -4,7 +4,7 @@ package io.github.ryntric;
  * Represents the result of a polling operation performed by an event poller.
  **/
 
-public enum PollState {
+enum PollState {
     PROCESSING,
     IDLE
 }
